1fb2b43da5c36876bd7353999cb5343da8993c14
crocokyle <me@kyle-dixon.com>
2022-11-04 22:36:20 -0400

    style: :pencil2: Fix set_personal typo

--- __main__.py
+++ __main__.py
@@ -13,13 +13,13 @@ def set_work(monitor):
     elif monitor_data.get('model') == Monitors.ASUS:
         monitor.set_input_source("HDMI1")
 
-def set_peronal(monitor):
+def set_personal(monitor):
     if (monitor_data := monitor.get_vcp_capabilities()).get('model') == Monitors.LG:
         monitor.set_input_source("DVI1")
     elif monitor_data.get('model') == Monitors.ASUS:
         monitor.set_input_source("DP1")
 
-def brute_force(monitor):
+def brute_force_inputs(monitor):
     for monitor in get_monitors():
         with monitor:
             if monitor.get_vcp_capabilities().get('model') == Monitors.LG:
