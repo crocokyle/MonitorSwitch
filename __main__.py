191062c86094a229427f48fa34706c817f0e44ad
crocokyle <me@kyle-dixon.com>
2022-11-04 22:31:14 -0400

    feat: :alembic: Found input source types for LG monitor

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-from monitorcontrol import get_monitors
+from monitorcontrol import get_monitors, InputSource
 
 import time
 
@@ -9,16 +9,27 @@ class Monitors:
 
 def set_work(monitor):
     if (monitor_data := monitor.get_vcp_capabilities()).get('model') == Monitors.LG:
-        monitor.set_input_source("DP2")
+        monitor.set_input_source("COMPOSITE1")
     elif monitor_data.get('model') == Monitors.ASUS:
         monitor.set_input_source("HDMI1")
 
 def set_peronal(monitor):
     if (monitor_data := monitor.get_vcp_capabilities()).get('model') == Monitors.LG:
-        monitor.set_input_source("HDMI1")
+        monitor.set_input_source("DVI1")
     elif monitor_data.get('model') == Monitors.ASUS:
         monitor.set_input_source("DP1")
 
-for monitor in get_monitors():
-    with monitor:
-        set_work(monitor)
+def brute_force(monitor):
+    for monitor in get_monitors():
+        with monitor:
+            if monitor.get_vcp_capabilities().get('model') == Monitors.LG:
+                for source in InputSource:
+                    if source != InputSource.OFF:
+                        monitor.set_input_source(source)
+                        print(source)
+                        time.sleep(10)
+
+if __name__ == '__main__':
+    for monitor in get_monitors():
+        with monitor:
+            set_work(monitor)
